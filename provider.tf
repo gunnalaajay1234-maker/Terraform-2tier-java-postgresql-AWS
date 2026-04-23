0853dd15df4d5ebaad4bb327614e87205bd84b88
Saibaba Kola <146579091+Saibaba-22@users.noreply.github.com>
2026-04-23 20:59:40 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,9 +10,10 @@ terraform {
 
 terraform {
   backend "s3" {
-    bucket = "my-bucket"
-    key    = "ec2-vpc/terraform.tfstate"
-    dynamodb_table = "terraform-lock-table"        # for state locking
+    bucket         = "saiterrastate"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
     encrypt        = true
+    dynamodb_table = "terraform-lock"
   }
 }
